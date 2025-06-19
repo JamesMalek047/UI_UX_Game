5dbb26a3af348418d1504ceb80ff01675e4f7566
James Malek <jmale047@uottawa.ca>
2025-06-19 10:48:38 -0400

    test

--- word-flash/src/App.jsx
+++ word-flash/src/App.jsx
@@ -1,19 +1,36 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// import Game from "./Game";
+
+// function App() {
+//   // return (
+
+//   //   <Router>
+//   //     <Routes>
+//   //       <Route path="/game" element={<Game />} />
+//   //     </Routes>
+//   //   </Router>
+//   // );
+//   return <Game />;
+
+// }
+
+// export default App;
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Game from "./Game";
 
 function App() {
-  // return (
-
-  //   <Router>
-  //     <Routes>
-  //       <Route path="/game" element={<Game />} />
-  //     </Routes>
-  //   </Router>
-  // );
-  return <Game />;
-
+  return (
+    <Router basename="/UI_UX_Game">
+      <Routes>
+        <Route path="/" element={<Game />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
